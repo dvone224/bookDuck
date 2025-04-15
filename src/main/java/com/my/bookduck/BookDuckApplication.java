2b1c9a91b440491e5a35be567fa4bdfd0c114ddd
dvone224 <dvone224@gmail.com>
2025-04-15 16:04:56 +0900

    Initial commit

---
+++ src/main/java/com/my/bookduck/BookDuckApplication.java
@@ -0,0 +1,13 @@
+package com.my.bookduck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookDuckApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookDuckApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/my/bookduck/BookDuckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my.bookduck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookDuckApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
