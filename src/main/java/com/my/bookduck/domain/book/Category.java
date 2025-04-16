e2448e316cd783ee236e27dd98d5d136e09e1ff4
dvone224 <dvone224@gmail.com>
2025-04-16 17:49:16 +0900

    feat 0416

--- src/main/java/com/my/bookduck/domain/book/Category.java
+++ src/main/java/com/my/bookduck/domain/book/Category.java
@@ -6,20 +6,35 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@ToString(exclude = "bookCategories")
 public class Category {
-    // 장르
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "category_id")
     private Long id;
 
     private String name;
 
+    // 상위 카테고리 (대분류 → NULL, 소분류 → 대분류의 ID)
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_id")
+    private Category parent;
+
+    // 하위 카테고리 목록 (소분류들)
+    @OneToMany(mappedBy = "parent")
+    private List<Category> children;
+
     @OneToMany(mappedBy = "category", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<BookCategory> bookCategories;
+
+    // 생성자 (편의 메서드)
+    public Category(String name, Category parent) {
+        this.name = name;
+        this.parent = parent;
+    }
 }
