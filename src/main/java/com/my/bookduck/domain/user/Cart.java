1f6f336365d7b64a6e557246f2395e65412cfda8
dvone224 <dvone224@gmail.com>
2025-05-01 10:17:54 +0900

    feat 0501

--- src/main/java/com/my/bookduck/domain/user/Cart.java
+++ src/main/java/com/my/bookduck/domain/user/Cart.java
@@ -1,6 +1,7 @@
 package com.my.bookduck.domain.user;
 
 import com.my.bookduck.domain.book.Book;
+import com.my.bookduck.domain.group.Group;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -11,20 +12,20 @@ import lombok.*;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @ToString(exclude = {"user", "book"})
 public class Cart {
-//    @Id
-//    @Column(name="user_id")
-//    private Long userId;
-//
-//    @Id
-//    @Column(name="book_id")
-//    private Long bookId;
+    @Id
+    @Column(name="user_id")
+    private Long userId;
 
     @Id
+    @Column(name="book_id")
+    private Long bookId;
+
+    @MapsId("userId")
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
 
-    @Id
+    @MapsId("bookId")
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "book_id")
     private Book book;
