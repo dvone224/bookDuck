7fc696f513ec583a0f2c6e67c09bbf1323014aa4
naeyeon Kang <ganggom2@gmail.com>
2025-05-02 17:57:14 +0900

    feat: 250502 update

---
+++ src/main/java/com/my/bookduck/controller/ImageController.java
@@ -0,0 +1,44 @@
+package com.my.bookduck.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+@Slf4j
+@Controller
+@RequestMapping("/image")
+@RequiredArgsConstructor
+public class ImageController {
+
+    @GetMapping("/getimg")
+    public ResponseEntity<byte[]> getImg(String fileName) {
+        log.info("Uploading image to " + fileName);
+        File file = new File("C:\\bookduckImg\\" + fileName);
+
+        ResponseEntity<byte[]> result = null;
+
+        try{
+            HttpHeaders header = new HttpHeaders();
+            header.add("Content-Type", Files.probeContentType(file.toPath()));
+            result = new ResponseEntity<>(FileCopyUtils.copyToByteArray(file), header, HttpStatus.OK);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return result;
+    }
+
+}
